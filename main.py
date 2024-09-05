3ab03cc8b90603a51555990e6a2fd3bb06aa0dd6
santidrox <92232615+santidrox@users.noreply.github.com>
2024-09-05 07:25:37 -0600

    Add files via upload
    Versión mejorada y con más funciones que la primera versión.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import os
 import csv
 import re
 
 app = Flask(__name__)
 
-# Cargar los datos del CSV en un diccionario
+
+IMAGE_FOLDER = 'img'
+AUDIO_FOLDER = 'audio'
+os.makedirs(AUDIO_FOLDER, exist_ok=True)
+
+app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+app.config['AUDIO_FOLDER'] = AUDIO_FOLDER
+
+
 def cargar_estudiantes():
     estudiantes = {}
     with open('Listado Cuarto Bachillerato.csv', mode='r', encoding='utf-8') as file:
@@ -14,7 +23,9 @@ def cargar_estudiantes():
             estudiantes[carnet] = {
                 'nombre': row['nombre'].strip(),
                 'apellido': row['apellido'].strip(),
-                'carrera': row['carrera'].strip()
+                'carrera': row['carrera'].strip(),
+                'imagen': f"{carnet}.jpg",
+                'audio': f"{carnet}.mp3"  
             }
     return estudiantes
 
@@ -26,25 +37,35 @@ def index():
 
 @app.route('/scan', methods=['POST'])
 def scan():
-    codigo_qr = request.json['codigo_qr'].strip()  # Eliminar espacios en blanco
+    codigo_qr = request.json['codigo_qr'].strip()
     
-    # Buscar el carnet en el texto escaneado usando una expresión regular
+   
     carnet_match = re.search(r'Carnet:\s*(\d+)', codigo_qr)
     if carnet_match:
-        codigo_qr = carnet_match.group(1)  # Extraer el número de carnet
+        codigo_qr = carnet_match.group(1)
 
-    # Buscar en el diccionario de estudiantes
     estudiante = estudiantes.get(codigo_qr, None)
     
     if estudiante:
+        audio_url = f"/audio/{estudiante['audio']}"
         return jsonify({
             'nombre': estudiante['nombre'],
             'apellido': estudiante['apellido'],
             'carrera': estudiante['carrera'],
-            'carnet': codigo_qr
+            'carnet': codigo_qr,
+            'imagen': f"/images/{estudiante['imagen']}",
+            'audio': audio_url
         })
     else:
         return jsonify({'error': 'Estudiante no encontrado'})
+
+@app.route('/images/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/audio/<filename>')
+def serve_audio(filename):
+    return send_from_directory(app.config['AUDIO_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
